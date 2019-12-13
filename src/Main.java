c1d58501e4d0f23c94df730be06c19a5f1896687
Mihai P <pintea.mihai@gmail.com>
2019-12-13 19:50:35 +0200

    JavaAdvance

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee bogdan = new Employee();
+        bogdan.name = "Bogdan";
+        bogdan.salary = 9000;
+        bogdan.age = 45;
+       int b =  bogdan.calcBonus();
+        System.out.println(b);
+    }
+}
